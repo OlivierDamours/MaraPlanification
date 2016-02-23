f428d0ddef62f6f4c90a368b10362a01b4d2feab
Jeff <whitearabqc@gmail.com>
2016-02-22 21:04:28 -0500

    JS for refresh on a tag click forgotten page

--- serviceConseil.php
+++ serviceConseil.php
@@ -73,7 +73,7 @@
           <div class="col-md-5">
             <img class="imgInvestisseurs" src="Images/investisseurs.jpg">
           </div>
-          
+
         </article>
       </div>
       <div>
@@ -132,6 +132,8 @@
     </footer>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <script src="js/script.js"> </script>
+
     <script>
     $(".nav li").on("click", function() {
       $(".nav li").removeClass("active");
